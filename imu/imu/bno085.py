be171429a2b297693064f4ef5a92a2ad51c68454
Henry Buron <henryburon34@gmail.com>
2024-05-09 21:12:00 -0500

    very basics of imu node set up

--- imu/imu/bno085.py
+++ imu/imu/bno085.py
@@ -1,6 +1,24 @@
-def main():
-    print('Hi from imu.')
+import rclpy
+from rclpy.node import Node
 
 
-if __name__ == '__main__':
-    main()
+
+class BNO085(Node):
+    def __init__(self):
+        super().__init__('bno085')
+
+        # Timers
+        self.timer = self.create_timer(1/100, self.timer_callback)
+
+    
+    def timer_callback(self):
+        self.get_logger().info('Hello World!')
+
+
+
+
+def bn085_entry(args=None):
+    rclpy.init(args=args)
+    node = BNO085()
+    rclpy.spin(node)
+    rclpy.shutdown()
