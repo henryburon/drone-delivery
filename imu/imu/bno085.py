5aa77548e9f0bed4aa2744eb8a30892f76239800
Henry Buron <henryburon34@gmail.com>
2024-05-15 16:55:57 -0500

    Publishing all the data from the IMU

--- imu/imu/bno085.py
+++ imu/imu/bno085.py
@@ -11,7 +11,8 @@ from adafruit_bno08x import (
     BNO_REPORT_LINEAR_ACCELERATION
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
-from sensor_msgs.msg import Imu, MagneticField #, Quaternion
+from sensor_msgs.msg import Imu, MagneticField
+from geometry_msgs.msg import QuaternionStamped, PointStamped
 
 
 class BNO085(Node):
@@ -37,32 +38,81 @@ class BNO085(Node):
         self.timer = self.create_timer(1/20, self.timer_callback)
 
         # Publishers
-        # could eventually make a custom message type, but this is simpler for now
         self.imu_pub = self.create_publisher(Imu, 'imu/data', 10)
-
+        self.mag_pub = self.create_publisher(MagneticField, 'imu/mag', 10)
+        self.quat_pub = self.create_publisher(QuaternionStamped, 'imu/quat', 10)
+        self.acc_pub = self.create_publisher(PointStamped, 'imu/acc', 10)
+        self.grav_pub = self.create_publisher(PointStamped, 'imu/grav', 10)
 
     def timer_callback(self):
 
-        # Read sensor values
-        accel_x, accel_y, accel_z = self.bno.acceleration # acceleration
+        # read sensor values each loop
+        acc_x, acc_y, acc_z = self.bno.acceleration # acceleration
         gyro_x, gyro_y, gyro_z = self.bno.gyro # gyro
         mag_x, mag_y, mag_z = self.bno.magnetic # magnetometer
         quat_i, quat_j, quat_k, quat_real = self.bno.quaternion # rotation vector quaternion
         grav_x, grav_y, grav_z = self.bno.gravity # gravity
         lin_acc_x, lin_acc_y, lin_acc_z = self.bno.linear_acceleration # linear acceleration
 
+        # make the IMU message
         imu_msg = Imu()
-        imu_msg.linear_acceleration.x = lin_acc_x
-        imu_msg.linear_acceleration.y = lin_acc_y
-        imu_msg.linear_acceleration.z = lin_acc_z
+
+        imu_msg.header.stamp = self.get_clock().now().to_msg()
+        
         imu_msg.angular_velocity.x = gyro_x
         imu_msg.angular_velocity.y = gyro_y
         imu_msg.angular_velocity.z = gyro_z
 
+        imu_msg.linear_acceleration.x = lin_acc_x
+        imu_msg.linear_acceleration.y = lin_acc_y
+        imu_msg.linear_acceleration.z = lin_acc_z
+
         self.imu_pub.publish(imu_msg)
 
-        self.get_logger().info("test")
+        # make the MagneticField message
+        mag_msg = MagneticField()
 
+        mag_msg.header.stamp = self.get_clock().now().to_msg()
+
+        mag_msg.magnetic_field.x = mag_x
+        mag_msg.magnetic_field.y = mag_y
+        mag_msg.magnetic_field.z = mag_z
+
+        self.mag_pub.publish(mag_msg)
+
+        # make the QuaternionStamped message
+        quat_msg = QuaternionStamped()
+
+        quat_msg.header.stamp = self.get_clock().now().to_msg()
+
+        quat_msg.quaternion.x = quat_i
+        quat_msg.quaternion.y = quat_j
+        quat_msg.quaternion.z = quat_k
+        quat_msg.quaternion.w = quat_real
+
+        self.quat_pub.publish(quat_msg)
+
+        # make the Acceleration (PointStamped) message
+        acc_msg = PointStamped()
+
+        acc_msg.header.stamp = self.get_clock().now().to_msg()
+
+        acc_msg.point.x = acc_x
+        acc_msg.point.y = acc_y
+        acc_msg.point.z = acc_z
+
+        self.acc_pub.publish(acc_msg)
+
+        # make the Gravity (PointStamped) message
+        grav_msg = PointStamped()
+
+        grav_msg.header.stamp = self.get_clock().now().to_msg()
+
+        grav_msg.point.x = grav_x
+        grav_msg.point.y = grav_y
+        grav_msg.point.z = grav_z
+        
+        self.grav_pub.publish(grav_msg)
 
 
 
