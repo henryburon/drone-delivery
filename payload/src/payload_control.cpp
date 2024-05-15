7d736f67e5dbd5a223989100842c9ba5c2c3311e
Henry Buron <henryburon34@gmail.com>
2024-05-15 18:09:08 -0500

    skeleton of payload control cpp node set up

--- payload/src/payload_control.cpp
+++ payload/src/payload_control.cpp
@@ -1,10 +1,50 @@
+#include "rclcpp/rclcpp.hpp"
+#include <chrono>
 #include <cstdio>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+using namespace std::chrono_literals; // enable the usage of 1s, 1ms, 1us, etc
 
-  printf("hello world payload package\n");
+class PayloadControl : public rclcpp::Node
+{
+public:
+  PayloadControl()
+  : Node("payload_control")
+  {
+    // Subscribers
+    
+
+    // Timers
+    timer_ =
+      this->create_wall_timer(0.01s, std::bind(&PayloadControl::timer_callback, this));
+  }
+
+private:
+
+  void timer_callback()
+  {
+    // log a simple message for testing
+    RCLCPP_INFO(this->get_logger(), "Hello, world!");
+  }
+
+
+  // Declare timers
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  // Declare subscribers
+
+
+
+
+};
+
+
+
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PayloadControl>());
+  rclcpp::shutdown();
   return 0;
 }
