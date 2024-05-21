cf4b14f6b78bf6b62ffaea4ffc8384283a9d2192
Henry Buron <henryburon34@gmail.com>
2024-05-21 17:14:27 -0500

    adding

--- imu/files/test_gpio.py
+++ imu/files/test_gpio.py
@@ -1,33 +1,29 @@
 import board
 import busio
 import adafruit_pca9685
-
-# Initialize I2C bus and PCA9685 module
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
 
-# Set the PWM frequency
+
+# this code is to control a brushless motor with ESC
+
+# set the frequency of the PWM signal to 50Hz
 pca.frequency = 50
-motor_channel = pca.channels[0]
 
-# Function to convert percentage to hex value
-def percent_to_duty_cycle(percent):
-    if not 0 <= percent <= 100:
-        raise ValueError("Percent must be between 0 and 100")
-    
-    # Calculate the corresponding duty cycle value
-    duty_cycle = int((percent / 100) * 0xFFFF)
-    
-    return duty_cycle
+# make the motor go forward
+pca.channels[0].duty_cycle = 0x7FFF
 
-percent = 5
-duty_cycle_value = percent_to_duty_cycle(percent)
+# wait for 2 seconds
+time.sleep(2)
 
-print(f"Duty cycle value for {percent}% is {duty_cycle_value}")
+# make the motor go backward
+pca.channels[0].duty_cycle = 0x1FFF
 
-# Set the motor channel's duty cycle in a loop
-while True:
-    motor_channel.duty_cycle = duty_cycle_value
+# wait for 2 seconds
+time.sleep(2)
 
-# while True:
-#    motor_channel.duty_cycle = 0xAFC8 # in decimal, this is 39000
+# stop the motor
+pca.channels[0].duty_cycle = 0x0000
+
+# wait for 2 seconds
+time.sleep(2)
