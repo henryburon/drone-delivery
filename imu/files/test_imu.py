a27ca7b2738d09d87a02206aea81bc8900fe6342
Henry Buron <henryburon34@gmail.com>
2024-05-20 23:55:15 -0500

    gyro

--- imu/files/test_imu.py
+++ imu/files/test_imu.py
@@ -13,8 +13,12 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 bno = BNO08X_I2C(i2c)
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+bno.enable_feature(BNO_REPORT_GYROSCOPE)
 
 
 while True:
       accel_x, accel_y, accel_z = bno.acceleration
       print(accel_x, accel_y, accel_z)
+
+      gyro_x, gyro_y, gyro_z = bno.gyro
+      print(gyro_x, gyro_y, gyro_z)
