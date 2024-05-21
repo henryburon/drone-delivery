dad174a8e40e046c62fd9b1a268c5b15289badfa
Henry Buron <henryburon34@gmail.com>
2024-05-20 22:48:34 -0500

    sda scl

--- imu/files/test_imu.py
+++ imu/files/test_imu.py
@@ -10,7 +10,7 @@ from adafruit_bno08x import (
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
 
-i2c = busio.I2C(board.SCL_1, board.SDA_1)
+i2c = busio.I2C(board.SCL, board.SDA)
 bno = BNO08X_I2C(i2c)
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 
