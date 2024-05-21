415b68f643182a472188a875bf9bc1737ab8e986
Henry Buron <henryburon34@gmail.com>
2024-05-21 17:43:42 -0500

    test

--- imu/files/test_gpio.py
+++ imu/files/test_gpio.py
@@ -14,17 +14,17 @@ pca = adafruit_pca9685.PCA9685(i2c)
 pca.frequency = 50
 
 # make the motor go forward
-led_channel = pca.channels[0]
+led_channel = pca.channels[2]
 
 # Increase brightness:
-# for i in range(0xffff):
-#     time.sleep(0.01)
-#     led_channel.duty_cycle = i
-#     print(i)
+for i in range(0xffff):
+    time.sleep(0.01)
+    led_channel.duty_cycle = i
+    print(i)
 
 
-while True:
-    led_channel.duty_cycle = 0xE10
+# while True:
+#     led_channel.duty_cycle = 0xE10
 
 # make a while loop that lasts 7 seconds
 
