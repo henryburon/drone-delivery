78bea022b1984dfa6ce8d5e9b7ff555baebe9740
Henry Buron <henryburon34@gmail.com>
2024-05-21 18:01:40 -0500

    test

--- imu/files/test_gpio.py
+++ imu/files/test_gpio.py
@@ -16,17 +16,17 @@ pca.frequency = 50
 # make the motor go forward
 led_channel = pca.channels[2]
 
-while True:
-      led_channel.duty_cycle = 0xDF9
-      time.sleep(5)
-      led_channel.duty_cycle = 0x000
-      time.sleep(1)
+# while True:
+#       led_channel.duty_cycle = 0xDF9
+#       time.sleep(5)
+#       led_channel.duty_cycle = 0x000
+#       time.sleep(1)
 
 
 # Increase brightness:
-# for i in range(0xffff):
-#     time.sleep(0.005)
-#     led_channel.duty_cycle = i
-#     print(i)
+for i in range(0, 0xffff, 2):
+    time.sleep(0.01)
+    led_channel.duty_cycle = i
+    print(i)
 
 
