24c08000dbd054567610cf0d75c1607ac9cd9fb0
Henry Buron <henryburon34@gmail.com>
2024-05-21 15:12:16 -0500

    function

--- imu/files/test_gpio.py
+++ imu/files/test_gpio.py
@@ -2,19 +2,32 @@ import board
 import busio
 import adafruit_pca9685
 
-print("1")
-
+# Initialize I2C bus and PCA9685 module
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
 
-print("2")
-
+# Set the PWM frequency
 pca.frequency = 50
 motor_channel = pca.channels[0]
 
-while True:
-   motor_channel.duty_cycle = 0x7fff
+# Function to convert percentage to hex value
+def percent_to_duty_cycle(percent):
+    if not 0 <= percent <= 100:
+        raise ValueError("Percent must be between 0 and 100")
+    
+    # Calculate the corresponding duty cycle value
+    duty_cycle = int((percent / 100) * 0xFFFF)
+    
+    return duty_cycle
 
+percent = 50
+duty_cycle_value = percent_to_duty_cycle(percent)
+
+print(f"Duty cycle value for {percent}% is {duty_cycle_value}")
+
+# Set the motor channel's duty cycle in a loop
+while True:
+    motor_channel.duty_cycle = duty_cycle_value
 
 # while True:
 #    motor_channel.duty_cycle = 0xAFC8 # in decimal, this is 39000
