ccca163059e82e06ebb71ae9bd1f6ccf980ef25a
Henry Buron <henryburon34@gmail.com>
2024-05-28 18:34:33 -0500

    test

--- imu/files/test_gpio.py
+++ imu/files/test_gpio.py
@@ -1,13 +1,3 @@
-##### NOTES #####
-
-# To Arm the ESC, I can do a range of 2800 to 3000, time.sleep 0.01, increment 2
-
-
-
-
-
-
-
 import board
 import busio
 import adafruit_pca9685
@@ -16,17 +6,12 @@ i2c = busio.I2C(board.SCL, board.SDA)
 pca = adafruit_pca9685.PCA9685(i2c)
 
 
-
-
-
-
-
 # set the frequency of the PWM signal to 50Hz
 pca.frequency = 50
 
 # make the motor go forward
 led_channel_0 = pca.channels[0]
-led_channel_2 = pca.channels[2]
+# led_channel_2 = pca.channels[2]
 
 # ARM the ESC first. This is entirely the ESC, not the PCA9685
 # to arm the ESC, the min range I can get it to work at is 2800 - 3000, skipping 2
@@ -34,7 +19,7 @@ led_channel_2 = pca.channels[2]
 for i in range(2800, 3000, 2): # safe: 2500 - 3000
    time.sleep(0.01)
    led_channel_0.duty_cycle = i
-   led_channel_2.duty_cycle = i
+   # led_channel_2.duty_cycle = i
    print(i)
 
 print("armed!")
@@ -45,7 +30,7 @@ while True:
    # ask the user to input an integer
    user_input = input("Enter a duty cycle (int): ")
    led_channel_0.duty_cycle = int(user_input)
-   led_channel_2.duty_cycle = int(user_input)
+   # led_channel_2.duty_cycle = int(user_input)
    print("Duty cycle set to: " + str(user_input))
    time.sleep(2)
 
